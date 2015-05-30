066b2e12c060d18de0d37f951a13cefa7b641760
Fábio Hahn <fabioaugustoti@gmail.com>
2015-05-29 21:17:36 -0400

    Incluindo opcoes no layout.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,16 +1,43 @@
 angular.module('touch.controllers', [])
 
-.controller('AvaliacaoCtrl', function($scope) {
+.controller('AvaliacaoCtrl', function($scope, $ionicActionSheet, $timeout, $ionicPopup) {
 	$scope.mostrarResultado = false;
 
 	var inicializarContadores = function(){
 		$scope.qtdVermelho = 0;
 		$scope.qtdAmarelo = 0;
-		$scope.qtdVerde = 0;	
+		$scope.qtdVerde = 0;
 	};
 	inicializarContadores();
 	
-	$scope.resetarContadores = function(){
-		inicializarContadores();
+	var esconderResultados = function(){
+		$scope.mostrarResultado = false;
 	};
+
+	$scope.mostrarDetalhes = function() {
+		$scope.mostrarResultado = true;
+
+		var hideSheet = $ionicActionSheet.show({
+	     titleText: 'Options',
+	     buttons: [
+	       { text: 'Save' }
+	     ],
+	     buttonClicked: function(index) {
+	       $ionicPopup.alert({
+		     title: 'Save Success'
+		   });
+		   esconderResultados();
+	       return true;
+	     },
+	     destructiveText: 'Reset',
+	     destructiveButtonClicked: function(){
+	     	inicializarContadores();
+	     	esconderResultados();
+	     },
+	     cancelText: 'Cancel',
+	     cancel: function() {
+			esconderResultados();
+	     }
+	   });
+ 	};
 });
